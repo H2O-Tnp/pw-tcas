db156abaee46e9eb5f942f7fb16f84017297a08c
NTMP <tanapatpanakorn@gmail.com>
2025-01-19 18:21:59 +0700

    I think it is the last one

--- app/api/register/route.ts
+++ app/api/register/route.ts
@@ -1,18 +1,27 @@
 import prisma from "#/lib/db";
 import { NextResponse, NextRequest } from "next/server";
 
-export async function POST(req: NextRequest){
-  const body = await req.json();
-  console.log("this is register api");
-  console.log(body);
-  await prisma.student.create({
-    data: {
-      name: body.name,
-      surname: body.surname,
-      email: body.email,
-      class: "",
-      academic_year: 0
-    }
-  })
-  return NextResponse.json({body});
+export async function POST(req: NextRequest) {
+  try {
+    const body = await req.json();
+    console.log("Register API called with body:", body);
+
+    await prisma.student.create({
+      data: {
+        name: body.name,
+        surname: body.surname,
+        email: body.email,
+        class: "", // Placeholder
+        academic_year: 0, // Placeholder
+      },
+    });
+
+    return NextResponse.json({ message: "User successfully registered" });
+  } catch (error) {
+    console.error("Error in Register API:", error);
+    return NextResponse.json(
+      { error: "Failed to register user" },
+      { status: 500 }
+    );
+  }
 }
